76e1932e9e662e159ff5eae3db4cf6192a1a4871
nandinijari <nandinijariwala01@gmail.com>
2024-08-07 20:37:06 -0400

    push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar(props) {
             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/about">About Us</Link>
+            <Link className="nav-link" to="/about">About</Link>
           </li>
      
         </ul>
